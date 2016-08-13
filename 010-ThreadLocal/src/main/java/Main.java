66fa7ac701c1ea5408815828053c2ee764c645f6
Grady0000 <stzhangjk@163.com>
2016-08-13 20:59:48 +0800

    ThreadLocal修改初始值

--- 010-ThreadLocal/src/main/java/Main.java
+++ 010-ThreadLocal/src/main/java/Main.java
@@ -2,7 +2,13 @@
  * Created by Grady on 2016.8.13.
  */
 public class Main {
-    public static ThreadLocal<String> tl = new ThreadLocal<>();
+    public static ThreadLocal<String> tl = new ThreadLocal<String>(){
+        @Override
+        protected String initialValue() {
+            //可修改初始值不为null
+            return super.initialValue();
+        }
+    };
 
     public static void main(String[] args) throws InterruptedException {
         new ThreadA().start();
